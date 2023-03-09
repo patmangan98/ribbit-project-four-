e20226095d731774d1986e5d82f546cebc901ba6
erika helm <erikadphelm16@gmail.com>
2023-03-09 13:33:59 -0600

    created models Honesty <honestymoore77@gmail.com>

---
+++ models/comment.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+    text:{
+        type: String,
+        required: true
+    },
+    owner:{
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+})
+
+module.exports = commentSchema
---
+++ models/post.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const commentSchema = require('./comment')
+
+
+const postSchema = new Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    text:{
+        type: String,
+        required: true
+    },
+    category:{
+        type: String,
+        required: true
+    },
+    thread:{
+        type: Schema.Types.ObjectId,
+        ref: 'Thread',
+        required: true
+    },
+    owner:{
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    comments:[commentSchema]
+})
+
+module.exports = mongoose.model('Post', postSchema)
