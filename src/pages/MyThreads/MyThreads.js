07c360e761ae1f33c1ef86a9fd9c9f485cc8c34d
Patrick Mangan <patrickmangan@Patricks-Mini.lan>
2023-03-13 13:20:34 -0400

    got myThreads page working.

--- src/pages/MyThreads/MyThreads.js
+++ src/pages/MyThreads/MyThreads.js
@@ -6,11 +6,35 @@ import CreateThread from '../components/CreateForms/CreateThread'
 
 export default function MyThreads({user}) {
 
+    let userThreads = []
+
+    function findUserThreads (arr) {
+        console.log(arr)
+        arr.forEach((thread) => {
+            if(thread.owner._id === user._id){
+                userThreads.push(thread)
+            }
+        })
+    }
+
     const [myThreadArr, setMyThreadArr] = useState([])
+
+    useEffect(() => {
+        indexThread()
+            .then((res) => res.json())
+            .then((resData) => setMyThreadArr(resData.threads))
+
+    }, [])
+
+    findUserThreads(myThreadArr)
+  
+    const threadMap = userThreads.map((thread, index) => (<ThreadCard thread={thread} key ={index}/>)) 
+
 
     return (
         <>
             <h2>Create Thread</h2>
+            {threadMap}
             {/* <button onClick={handleCheckToken}>Check When My Login Expires</button> */}
         </>
         )
