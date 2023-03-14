eb721aad093d373567be6a4dbc361299c1ed57fd
Heather Knapp <heather@heathers-air.mynetworksettings.com>
2023-03-14 11:51:25 -0400

    practice menu

--- src/pages/components/NavBar/NavBar.js
+++ src/pages/components/NavBar/NavBar.js
@@ -1,9 +1,21 @@
 import { Link } from "react-router-dom";
+import {useState} from "react"
 import * as userService from '../../../utilities/users-service'
+import './NavBar.css'
+import {
+	MDBContainer,
+	MDBCollapse,
+	MDBNavbar,
+	MDBNavbarToggler,
+	MDBIcon,
+	MDBBtn,
+  } from 'mdb-react-ui-kit'
 
 export default function NavBar({ user, setUser }) {
 
-	function handleLogOut () {
+	const [navMenu, setNavMenu] = useState(false)
+
+	function handleLogOut() {
 		// we should delgate the actual logging out to the users service
 		userService.logOut()
 		setUser(null)
@@ -11,14 +23,30 @@ export default function NavBar({ user, setUser }) {
 
 	return (
 		<>
-		<nav className="navbar bg-dark fluid py-3">
-			<Link className="nav-link text-white" to='/mythreads'>My Threads</Link>
-            <Link className="nav-link text-white" to='/global'>Global Page</Link>
-			<Link className="nav-link text-white" to="" onClick={handleLogOut}>Log Out</Link>
-		</nav>
-		<>
-		<span className="mt-3">Welcome, { user.name }</span>
-		</>
+			<nav className="navbar bg-dark fluid py-2 d-flex">
+				<div className="nav-link " id="logo-name">ribbit</div>
+				<div className="menu-container justify-content-end">
+				<div className="navbar-menu">
+						<button
+							type="button"
+							class="btn btn-secondary dropdown-toggle"
+							data-toggle="dropdown"
+							aria-haspopup="true"
+							aria-expanded="false"
+							id="collapse"
+							onClick={() => setNavMenu((prevState) => !prevState)}
+						>
+							<i className="bi bi-list"></i>
+						</button>
+					</div>
+				</div>
+				<Link className="nav-link text-white" to='/mythreads'>My Threads</Link>
+				<Link className="nav-link text-white" to='/global'>Global Page</Link>
+				<Link className="nav-link text-white" to="" onClick={handleLogOut}>Log Out</Link>
+			</nav>
+			<>
+				<span className="mt-3">Welcome, {user.name}</span>
+			</>
 		</>
 	)
 }
